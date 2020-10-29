2bcab2ab6b50f49bb1575a4e96ab010e749a4b87
Brenda Ferrufino <ferrufinob@gmail.com>
2020-10-28 21:53:27 -0400

    Done.

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,8 +1,8 @@
 # write a migration to add a column, season, tot he shows table. string column
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
     
-    # def change 
-    #     add_column :shows, :season, :string
-    # end
+    def change 
+        add_column :shows, :season, :string
+    end
 
 end
